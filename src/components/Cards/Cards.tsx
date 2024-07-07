d46ab16307e6b4e1e38898071f371840a5d0f344
AmletixQ <nazarovamridin308@gmail.com>
2024-07-07 20:18:48 +0300

    Some fixes

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -17,7 +17,7 @@ const Cards = () => {
             <h3 className="bc-green">Search engine optimization</h3>
           </Card.Title>
           <Link>
-            <Link.Arrow circled={"black"} simple={"green"} />
+            <Link.Arrow circled={"black"} simple={"green"} className="arrow" />
             <Link.P />
           </Link>
         </Card.Content>
@@ -29,7 +29,7 @@ const Cards = () => {
             <h3 className="bc-gray">Pay-per-click advertising</h3>
           </Card.Title>
           <Link>
-            <Link.Arrow circled={"black"} simple={"green"} />
+            <Link.Arrow circled={"black"} simple={"green"} className="arrow" />
             <Link.P />
           </Link>
         </Card.Content>
@@ -41,7 +41,7 @@ const Cards = () => {
             <h3 className="bc-gray">Social Media Marketing</h3>
           </Card.Title>
           <Link color={"white"}>
-            <Link.Arrow circled={"white"} simple={"black"} />
+            <Link.Arrow circled={"white"} simple={"black"} className="arrow" />
             <Link.P />
           </Link>
         </Card.Content>
@@ -53,7 +53,7 @@ const Cards = () => {
             <h3 className="bc-green">Email Marketing</h3>
           </Card.Title>
           <Link>
-            <Link.Arrow circled={"black"} simple={"green"} />
+            <Link.Arrow circled={"black"} simple={"green"} className="arrow" />
             <Link.P />
           </Link>
         </Card.Content>
@@ -65,7 +65,7 @@ const Cards = () => {
             <h3 className="bc-gray">Content Creation</h3>
           </Card.Title>
           <Link>
-            <Link.Arrow circled={"black"} simple={"green"} />
+            <Link.Arrow circled={"black"} simple={"green"} className="arrow" />
             <Link.P />
           </Link>
         </Card.Content>
@@ -77,7 +77,7 @@ const Cards = () => {
             <h3 className="bc-green">Analytics and Tracking</h3>
           </Card.Title>
           <Link color={"white"}>
-            <Link.Arrow circled={"white"} simple={"black"} />
+            <Link.Arrow circled={"white"} simple={"black"} className="arrow" />
             <Link.P />
           </Link>
         </Card.Content>

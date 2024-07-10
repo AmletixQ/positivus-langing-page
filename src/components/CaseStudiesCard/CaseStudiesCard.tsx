5813748159dd78805b14c3adf99e890cd14e9319
AmletixQ <nazarovamridin308@gmail.com>
2024-07-10 15:56:19 +0300

    Fix

--- src/components/CaseStudiesCard/CaseStudiesCard.tsx
+++ src/components/CaseStudiesCard/CaseStudiesCard.tsx
@@ -10,7 +10,6 @@ const CaseStudiesCard = () => {
           resulted in a 50% increase in website traffic and a 25% increase in
           sales.
         </p>
-
         <Link>
           <Link.P style={{ color: "var(--green)" }} />
           <Link.Arrow simple={"green"} />
@@ -19,11 +18,10 @@ const CaseStudiesCard = () => {
       <hr />
       <div className="card-item">
         <p>
-          For a local restaurant, we implemented a targeted PPC campaign that
-          resulted in a 50% increase in website traffic and a 25% increase in
-          sales.
+          For a B2B software company, we developed an SEO strategy that resulted
+          in a first page ranking for key keywords and a 200% increase in
+          organic traffic.
         </p>
-
         <Link>
           <Link.P style={{ color: "var(--green)" }} />
           <Link.Arrow simple={"green"} />
@@ -32,11 +30,10 @@ const CaseStudiesCard = () => {
       <hr />
       <div className="card-item">
         <p>
-          For a local restaurant, we implemented a targeted PPC campaign that
-          resulted in a 50% increase in website traffic and a 25% increase in
-          sales.
+          For a national retail chain, we created a social media marketing
+          campaign that increased followers by 25% and generated a 20% increase
+          in online sales.
         </p>
-
         <Link>
           <Link.P style={{ color: "var(--green)" }} />
           <Link.Arrow simple={"green"} />
